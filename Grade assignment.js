229f74b553555fa69ebc42326251e4f0ab8a7d76
Jude-raganit <juderaganit56@gmail.com>
2024-10-10 21:47:30 +0800

    Create Grade assignment.js

---
+++ Grade assignment.js
@@ -0,0 +1,10 @@
+let grade = 85;
+    if(grade >= 90){
+      console.log("excellent");
+  } else if( grade >= 80){
+      console.log("good");
+  } else if (grade>=70){
+      console.log("fair");
+  } else{
+      console.log("need improvement");
+  }
